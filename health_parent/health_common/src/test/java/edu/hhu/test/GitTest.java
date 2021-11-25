b30c83cfd01f0154daf597c40791f85e5b222109
godyu <1501042134@qq.com>
2021-11-25 16:35:12 +0800

    修改GitTest第7行代码 111111111 to 111111111master

--- health_parent/health_common/src/test/java/edu/hhu/test/GitTest.java
+++ health_parent/health_common/src/test/java/edu/hhu/test/GitTest.java
@@ -4,6 +4,6 @@ package edu.hhu.test;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("111111111");
+        System.out.println("111111111master");
     }
 }
